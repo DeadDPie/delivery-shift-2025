1563abd21f7b0cd30dfa1374386a2ee8af463c9b
DeadDPie <elizabethfox434@gmail.com>
2025-01-28 21:11:14 +0700

    fix: clean code

--- components/shared/ReactQueryClientProvider.tsx
+++ components/shared/ReactQueryClientProvider.tsx
@@ -1,25 +1,27 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { useState } from "react";
 
 export const ReactQueryClientProvider = ({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) => {
-	const [queryClient] = useState(
-		() =>
-			new QueryClient({
-				defaultOptions: {
-					queries: {
-
-						staleTime: 60 * 1000,
-					},
-				},
-			})
-	);
-	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-	);
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        staleTime: 60 * 1000,
+                    },
+                },
+            })
+    );
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
 };
