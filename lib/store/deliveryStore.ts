f85981ab53891409a94753df177aac05069345d0
DeadDPie <elizabethfox434@gmail.com>
2025-01-31 02:40:52 +0700

    feat: add persist

--- lib/store/deliveryStore.ts
+++ lib/store/deliveryStore.ts
@@ -1,18 +1,29 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface DeliveryData {
     params?: any;
     response?: any;
 }
 
-const useDeliveryStore = create<{
+interface DeliveryState {
     data: DeliveryData;
     setData: (newData: DeliveryData) => void;
     clearData: () => void;
-}>((set) => ({
-    data: {},
-    setData: (newData) => set({ data: newData }),
-    clearData: () => set({ data: {} }),
-}));
+}
+
+const useDeliveryStore = create<DeliveryState>()(
+    persist<DeliveryState>(
+        (set) => ({
+            data: {},
+            setData: (newData) => set({ data: newData }),
+            clearData: () => set({ data: {} }),
+        }),
+        {
+            name: "delivery-store",
+            storage: createJSONStorage<DeliveryState>(() => sessionStorage),
+        }
+    )
+);
 
 export default useDeliveryStore;
