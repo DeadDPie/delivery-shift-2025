6321f3d689aadb158300a149d0ed7ec5d66af0a0
DeadDPie <elizabethfox434@gmail.com>
2025-02-06 20:51:33 +0700

    feat: rename props

--- lib/contexts/ReactQueryClientProvider.tsx
+++ lib/contexts/ReactQueryClientProvider.tsx
@@ -4,13 +4,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { useState } from "react";
 
-interface ReactQueryClientProviderProp {
+interface ReactQueryClientProviderProps {
     children: React.ReactNode;
 }
 
 export const ReactQueryClientProvider = ({
     children,
-}: ReactQueryClientProviderProp) => {
+}: ReactQueryClientProviderProps) => {
     const [queryClient] = useState(
         () =>
             new QueryClient({
